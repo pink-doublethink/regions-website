a74142e46d2d9a1efb786c47a9b58065521e7fb1
Alexey Ovsyannikov <soliterco@gmail.com>
2023-07-18 15:39:18 +0300

    added prettier, eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import filesize from 'rollup-plugin-filesize'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    filesize({
+      format: {
+        exponent: 2,
+        spacer: ' ',
+        standard: 'iec'
+      }
+    }),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+    react(), 
+  ],
 })
